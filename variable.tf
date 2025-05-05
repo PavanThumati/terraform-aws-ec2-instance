4276162debdb63a0f1c6c1a31b21e86eb0ed8261
PavanThumati <pavanthumati999@gmail.com>
2025-05-05 13:01:55 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "module-ami" {
+  type = string
+}
+
+variable "module-instance_type" {
+  type = string
+}
+
+variable "module-subnet-id" {
+  type = string
+}
+variable "module-ec2-tag-name" {
+  type = string
+}
